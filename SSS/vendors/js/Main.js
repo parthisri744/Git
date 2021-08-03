018fd58bb6c01fd16cda4d2684db059b536f56c3
Parthiban <61075318+parthisri744@users.noreply.github.com>
2021-08-03 18:23:01 +0530

    Add files via upload

---
+++ SSS/vendors/js/Main.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict'
+  var forms = document.querySelectorAll('.needs-validation')
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+var app =angular.module("ucensss",[]);
+app.controller("sssctrl",function($scope){
+
+});
